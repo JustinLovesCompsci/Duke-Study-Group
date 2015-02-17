086630c0dfa8dda67930f15bf7608a1c237097f3
JustinLovesCompsci <justinzihaozhang@gmail.com>
2015-02-17 16:23:50 -0500

    set up welcome and register gui

---
+++ DukeStudy/WelcomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WelcomeViewController.swift
+//  DukeStudy
+//
+//  Created by Justin (Zihao) Zhang on 2/17/15.
+//  Copyright (c) 2015 Duke-Student-Government. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class WelcomeViewController:UIViewController {
+    
+}
